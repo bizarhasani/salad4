8848b929fc8fb119caca4298227ca62c5a8516af
bizarhasani <136457920+bizarhasani@users.noreply.github.com>
2023-07-26 07:11:56 +0400

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xdd1ee781d55b7ac824186f2c3afa95f8f066994a";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
